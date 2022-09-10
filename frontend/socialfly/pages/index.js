23e478c0fad0722f08d018967caebb27e25a3f72
Malek <josh@example.com>
2022-09-10 12:11:14 -0400

    Added images and fixed format on main page

--- frontend/socialfly/pages/index.js
+++ frontend/socialfly/pages/index.js
@@ -1,18 +1,24 @@
 import SearchBar from "../components/SearchBar";
 
+import PhoneIcon from "../components/phoneImage";
+import MacImage from "../components/macImage";
+
 export default function Home() {
   return (
-    <main className="bg-bage h-screen w-screen flex items-center flex-row justify-around">
+    <main className="bg-bage h-screen w-screen flex items-center flex-row justify-center space-x-20">
       <div>
-        <h1 className="text-7xl font-inter font-bold bg-clip-text bg-gradient-to-r from-light-blue to-dark-blue text-transparent">
+        <h1 className="text-7xl font-inter font-bold pb-2 sky-gradient">
           SocialFly
         </h1>
-        <h3 className="font-inter text-lg font-bold text-cement-gray">
+        <h3 className="pb-8 font-inter text-lg font-bold text-cement-gray">
           Soar through social media - one search at a time.
         </h3>
-        <SearchBar />
+        <SearchBar/>
       </div>
-      <div>Images</div>
+      <div className="relative ">
+        <PhoneIcon className="w-3/6 absolute -left-24 bottom-4 opacity-90" />
+        <MacImage className="w-[400px]"/>
+      </div>
     </main>
   );
 }
