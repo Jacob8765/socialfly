68298454bbfbd45d7202aac41fa278926cf3d968
Jacob Schuster <jschuster8765@outlook.com>
2022-09-10 14:30:35 -0400

    final nlp

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,7 @@
 const express = require("express")
 const axios = require("axios")
-const tf = require("@tensorflow/tfjs")
-const modelUtil = require("./model/util")
-const path = require("path");
-
-//import sentiment model
-const model = require("./model/model.json")
-const metadata = require("./model/metadata.json")
+const natural = require("natural")
+const {preProcess} = require("./model/util.js")
 
 const API_SECRET = "FLARGffUCG6uvE0LbT21q4I5YPyLw2UAqbb1fkU1PpvQSpmKuk"
 const BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAMasgwEAAAAA3gWGg7zbD%2BpENv2AN0qEW61KQ4I%3DNWFlE482Xh0JY12OvtOX1o4K1F2qLDN4pOVSt6yrCU2Xh6sQP1"
@@ -15,47 +10,13 @@ const API_KEY = "WVP6eAVUNaHHf3w7NSY0FxBGo"
 const app = express()
 var sentimentModel = null
 
-const loadModel = async () => {
-  console.log("loading model...")
-  try {
-    let url = 'https://storage.googleapis.com/tfjs-models/tfjs/sentiment_cnn_v1/model.json'
-    console.log("URL", url)
-    sentimentModel = await tf.loadLayersModel(url)
-  } catch (e) {
-    console.log(e)
-    throw "Error loading model"
-  }
-}
 
 const analyzeTweet = async (tweet) => {
-  if (!sentimentModel) { //Load the model if it's not already
-    try {
-      console.log("model is null")
-      await loadModel()
-    } catch (e) {
-      throw e
-    }
-  }
+  let preProcessedTweet = preProcess(tweet)
 
-  let tweet_text = tweet.replace(/(?:https?|ftp):\/\/[\n\S]+/g, ''); //remove all links
-  tweet_text = tweet_text.trim().toLowerCase().replace(/(\.|\,|\!)/g, '').split(' '); //more sanitization
-    // Convert the words to a sequence of word indices.
-    const sequence = tweet_text.map(word => {
-      let wordIndex = metadata.word_index[word] + metadata.index_from;
-      if (wordIndex > metadata.vocabulary_size) {
-        wordIndex = OOV_INDEX;
-      }
-      return wordIndex;
-    });
-    // Perform truncation and padding.
-    const paddedSequence = modelUtil.padSequences([sequence], metadata.max_len);
-    const input = tf.tensor2d(paddedSequence, [1, metadata.max_len]);
- 
-    const predictOut = sentimentModel.predict(input);
-    const score = predictOut.dataSync()[0];
-    predictOut.dispose();
-
-    return score
+  const Sentianalyzer = new natural.SentimentAnalyzer('English', natural.PorterStemmer, 'afinn');
+  const analysis_score = Sentianalyzer.getSentiment(preProcessedTweet);
+  console.log("Sentiment Score: ",analysis_score);
 }
 
 const getLatestTweets = async (keywords) => {
@@ -94,7 +55,7 @@ app.get("/getTweets", async (req, res) => {
 })
 
 app.listen(5000, async () => {
-  let sentiment = await analyzeTweet("")
+  let sentiment = await analyzeTweet("i love and hate the iphone!")
   console.log(sentiment)
 
   console.log("app live on port 5000")
--- backend/model/util.js
+++ backend/model/util.js
@@ -1,31 +1,89 @@
-function padSequences(
-  sequences, maxLen, padding = 'pre', truncating = 'pre', value = 0) {
-  // TODO(cais): This perhaps should be refined and moved into tfjs-preproc.
-  return sequences.map(seq => {
-    // Perform truncation.
-    if (seq.length > maxLen) {
-      if (truncating === 'pre') {
-        seq.splice(0, seq.length - maxLen);
-      } else {
-        seq.splice(maxLen, seq.length - maxLen);
-      }
-    }
-
-    // Perform padding.
-    if (seq.length < maxLen) {
-      const pad = [];
-      for (let i = 0; i < maxLen - seq.length; ++i) {
-        pad.push(value);
-      }
-      if (padding === 'pre') {
-        seq = pad.concat(seq);
-      } else {
-        seq = seq.concat(pad);
-      }
-    }
-
-    return seq;
-  });
+const stopword = require("stopword");
+const natural = require("natural")
+  
+// For conversion of contractions to standard lexicon
+const wordDict = {
+    "aren't": "are not",
+    "can't": "cannot",
+    "couldn't": "could not",
+    "didn't": "did not",
+    "doesn't": "does not",
+    "don't": "do not",
+    "hadn't": "had not",
+    "hasn't": "has not",
+    "haven't": "have not",
+    "he'd": "he would",
+    "he'll": "he will",
+    "he's": "he is",
+    "i'd": "I would",
+    "i'd": "I had",
+    "i'll": "I will",
+    "i'm": "I am",
+    "isn't": "is not",
+    "it's": "it is",
+    "it'll": "it will",
+    "i've": "I have",
+    "let's": "let us",
+    "mightn't": "might not",
+    "mustn't": "must not",
+    "shan't": "shall not",
+    "she'd": "she would",
+    "she'll": "she will",
+    "she's": "she is",
+    "shouldn't": "should not",
+    "that's": "that is",
+    "there's": "there is",
+    "they'd": "they would",
+    "they'll": "they will",
+    "they're": "they are",
+    "they've": "they have",
+    "we'd": "we would",
+    "we're": "we are",
+    "weren't": "were not",
+    "we've": "we have",
+    "what'll": "what will",
+    "what're": "what are",
+    "what's": "what is",
+    "what've": "what have",
+    "where's": "where is",
+    "who'd": "who would",
+    "who'll": "who will",
+    "who're": "who are",
+    "who's": "who is",
+    "who've": "who have",
+    "won't": "will not",
+    "wouldn't": "would not",
+    "you'd": "you would",
+    "you'll": "you will",
+    "you're": "you are",
+    "you've": "you have",
+    "'re": " are",
+    "wasn't": "was not",
+    "we'll": " will",
+    "didn't": "did not"
 }
 
-module.exports = {padSequences}
+const preProcess = text => {
+  let data = text.toLowerCase().replace(/[^a-zA-Z\s]+/g, '').split(' ');
+  data.forEach((word, index) => {
+      Object.keys(wordDict).forEach(key => {
+          if (key === word.toLowerCase()) {
+              data[index] = wordDict[key]
+          };
+      });
+  });
+
+  data = data.join(' ');
+
+  const tokenConstructor = new natural.WordTokenizer();
+  const tokenizedData = tokenConstructor.tokenize(data);
+  console.log("Tokenized Data: ",tokenizedData);
+
+  // Remove Stopwords
+  const filteredData = stopword.removeStopwords(tokenizedData);
+  console.log("After removing stopwords: ",filteredData);
+
+  return filteredData
+}
+
+module.exports = {preProcess}
