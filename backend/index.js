5f8aa695bb264db299738e33fae321edf248badc
Jacob Schuster <jschuster8765@outlook.com>
2022-09-10 10:27:41 -0400

    don't use the twitter library

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,33 @@
 const express = require("express")
-const { Client } = require("twitter-api-sdk");
+const axios = require("axios")
 
 const API_SECRET = "FLARGffUCG6uvE0LbT21q4I5YPyLw2UAqbb1fkU1PpvQSpmKuk"
 const BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAMasgwEAAAAA3gWGg7zbD%2BpENv2AN0qEW61KQ4I%3DNWFlE482Xh0JY12OvtOX1o4K1F2qLDN4pOVSt6yrCU2Xh6sQP1"
 const API_KEY = "WVP6eAVUNaHHf3w7NSY0FxBGo"
-const client = new Client(BEARER_TOKEN);
+const url = `https://api.twitter.com/2/tweets/search/recent?query=from%3ATwitterDev&tweet.fields=text&place.fields=country,geo,id`
 
 const app = express()
 
 app.get("/", async (req, res) => {
-  const tweet = await client.tweets.findTweetById("20");
-  console.log(tweet.data.text);
+  res.json({status: 200, version: 0.1})
+})
 
-  res.json(tweet)
+app.get("/getTweets", async (req, res) => {
+
+  axios.get(url, {
+    headers: {
+      authorization: `Bearer ${BEARER_TOKEN}`,
+    }
+  })
+  .then(function (response) {
+    console.log(response);
+    res.json(response.data)
+  })
+  .catch(function (error) {
+    console.log(error);
+    res.sendStatus(500)
+  });
+
 })
 
 app.listen(5000, () => {
