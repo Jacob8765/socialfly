7213707d669d21815100b93f64d04cfa561cff4d
Jacob Schuster <jschuster8765@outlook.com>
2022-09-10 10:37:47 -0400

    make the keywords work

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,6 @@ const axios = require("axios")
 const API_SECRET = "FLARGffUCG6uvE0LbT21q4I5YPyLw2UAqbb1fkU1PpvQSpmKuk"
 const BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAMasgwEAAAAA3gWGg7zbD%2BpENv2AN0qEW61KQ4I%3DNWFlE482Xh0JY12OvtOX1o4K1F2qLDN4pOVSt6yrCU2Xh6sQP1"
 const API_KEY = "WVP6eAVUNaHHf3w7NSY0FxBGo"
-const url = `https://api.twitter.com/2/tweets/search/recent?query=from%3ATwitterDev&tweet.fields=text&place.fields=country,geo,id`
 
 const app = express()
 
@@ -13,6 +12,10 @@ app.get("/", async (req, res) => {
 })
 
 app.get("/getTweets", async (req, res) => {
+  console.log(req.query.keywords)
+  let keywords = req.query.keywords.replace(",", " ")
+
+  const url = `https://api.twitter.com/2/tweets/search/recent?query=(${keywords})&tweet.fields=text&place.fields=country,geo,id`
 
   axios.get(url, {
     headers: {
@@ -20,7 +23,7 @@ app.get("/getTweets", async (req, res) => {
     }
   })
   .then(function (response) {
-    console.log(response);
+    //console.log(response);
     res.json(response.data)
   })
   .catch(function (error) {
